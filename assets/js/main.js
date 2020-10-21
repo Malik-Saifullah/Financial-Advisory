c30dcbf2e91d267c56c821056903c1b5395ca2b4
Malik Saifullah <maliksaifullah282@gmail.com>
2020-10-21 19:28:02 +0500

    creating filter menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,36 @@ $(document).ready(function () {
         $("#income-expense-toggle").addClass("income-expense-toggle");
     });
 });
+
+
+
+
+
+/*=======================================================
+                    dropdown with checkbox
+=======================================================*/
+$(".multiple_select").mousedown(function (e) {
+    if (e.target.tagName == "OPTION") {
+        return; //don't close dropdown if i select option
+    }
+    $(this).toggleClass('multiple_select_active'); //close dropdown if click inside <select> box
+});
+
+$(".multiple_select").on('blur', function (e) {
+    $(this).removeClass('multiple_select_active'); //close dropdown if click outside <select>
+});
+
+$('.multiple_select option').mousedown(function (e) { //no ctrl to select multiple
+    e.preventDefault();
+    $(this).prop('selected', $(this).prop('selected') ? false : true); //set selected options on click
+    $(this).parent().change(); //trigger change event
+});
+
+$("#myFilter").on('change', function () {
+    var selected = $("#myFilter").val().toString(); //here I get all options and convert to string
+    var document_style = document.documentElement.style;
+    if (selected !== "")
+        document_style.setProperty('--text', "'" + " " + selected + " " + "'");
+    else
+        document_style.setProperty('--text', "'Select values'");
+});
